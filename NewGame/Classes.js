1156b20d50a2657031a75c3a86b67c474d3859b1
Dean Orenstein <dto3576@gmail.com>
2019-10-13 23:30:01 -0500

    got board configured and showing up, developed Classes.js a bit

--- NewGame/Classes.js
+++ NewGame/Classes.js
@@ -34,3 +34,96 @@
 
     Winner is declared if he got the other's king
 */
+
+class Pawn {
+        constructor() {
+                this.
+        }
+
+        attack() {
+
+        }
+
+        firstMove() {
+
+        }
+
+        move() {
+
+        }
+}
+
+class Bishop {
+        constructor() {
+
+        }
+
+        attack() {
+
+        }
+
+        move() {
+
+        }
+
+}
+
+class Knight {
+        constructor() {
+
+        }
+
+        attack() {
+
+        }
+
+        move() {
+
+        }
+}
+
+class Rook {
+        constructor() {
+
+        }
+
+        attack() {
+
+        }
+
+        move() {
+
+        }
+}
+
+class King {
+        constructor() {
+
+        }
+
+        attack() {
+
+        }
+
+        move() {
+
+        }
+
+        captured() {  // If this is called the game ends
+
+        }
+}
+
+class Queen {
+        constructor() {
+
+        }
+
+        attack() {
+
+        }
+
+        move() {
+
+        }
+}
